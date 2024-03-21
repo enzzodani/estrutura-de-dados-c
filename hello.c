bf9ada9ead5cb86940e427ff337207ed0dff2eda
enzzodani <gomes.enzodani@gmail.com>
2024-03-21 17:01:39 -0300

    first commit

---
+++ hello.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ponto {
+	int x;
+	int y;
+};
+
+struct novo_ponto {
+	int x;
+	int y;
+};
+
+int main(){
+	struct ponto p1, p2= {1,2};
+	struct novo_ponto p3= {3,4};
+	
+	p1 = p2;
+	printf("p1 = %d e %d", p1.x, p1.y);
+	
+	system("pause");
+	return 0;
+}
