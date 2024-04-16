03d349b2634a9210ae79e2493d37e79cdf70f8b7
enzzodani <gomes.enzodani@gmail.com>
2024-04-16 11:58:15 -0300

    cod1 function and struct declaration

--- lista1/cod1.c
+++ lista1/cod1.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Declaração de Função
+float notaCaso1(struct dadosAluno aluno);
+
+float notaCaso2(struct dadosAluno aluno);
+
+float notaCaso3(struct dadosAluno aluno);
+
+float notaCaso4(struct dadosAluno aluno);
+
+float mediaGeral();
+
+float achaMaiorMedia();
+
+float achaMenorMedia();
+
+void achaMelhorAluno();
+
+void achaPiorAluno();
+
+//Definição de struct 
+struct dadosAluno {
+  unsigned int matrícula;
+  unsigned float p1;
+  unsigned float p2;
+  unsigned float p3;
+  unsigned float p4;
+};
+
 int main(int argc, char *argv[])
 {
-  
-  
+  //Declaração de variáveis
+  unsigned int numeroDeAlunos;
 
   return 0;
 }
+
+//Definição de Funções
