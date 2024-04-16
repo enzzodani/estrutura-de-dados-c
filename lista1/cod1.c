60fe162ae276c0cd2ee9b813b23cad05dfc5f09e
enzzodani <gomes.enzodani@gmail.com>
2024-04-16 12:23:37 -0300

    cod1 better struct

--- lista1/cod1.c
+++ lista1/cod1.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 //Declaração de Função
+void recebeNota();
+
 float notaCaso1(struct dadosAluno aluno);
 
 float notaCaso2(struct dadosAluno aluno);
@@ -23,16 +25,30 @@ void achaPiorAluno();
 //Definição de struct 
 struct dadosAluno {
   unsigned int matrícula;
-  unsigned float p1;
-  unsigned float p2;
-  unsigned float p3;
-  unsigned float p4;
+  float notas[4];
 };
 
 int main(int argc, char *argv[])
 {
   //Declaração de variáveis
   unsigned int numeroDeAlunos;
+
+  //Coleta
+  printf("Entre com o numero de alunos da turma: ");
+    scanf("%d", &numeroDeAlunos);
+  printf("\n");
+
+  //Verificação do numero de alunos
+  if (numeroDeAlunos <= 0 || numero > 50) {
+    printf("O numero de alunos na turma deve ser maior do que zero e menor ou igual a 50\n");
+    printf("Entre com o numero de alunos da turma: ");
+      scanf("%d", &numeroDeAlunos);
+    printf("\n");
+  }
+
+  
+
+  struct dadosAluno *alunos = malloc(sizeof())
 
   return 0;
 }
