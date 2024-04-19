bdf5f07d5f80551283b1f0651e47e0394f78d2cb
enzzodani <gomes.enzodani@gmail.com>
2024-04-19 10:09:52 -0300

    cod3 pont100 new variables

--- lista1/codigoConjunto/cod3oficial.c
+++ lista1/codigoConjunto/cod3oficial.c
@@ -9,47 +9,47 @@ int estaPresente(int vetor[], int tamanho, int elemento) {
     }
     return 0;
 }
-void uniao(int array1[], int size1, int array2[], int tamanho2, int resultado[], int *tamanhoResultado) {
+void uniao(int array1[], int size1, int array2[], int size2, int resultado[], int *sizeResult) {
     int i;
     for (i = 0; i < size1; i++) {
-        resultado[*tamanhoResultado] = array1[i];
-        (*tamanhoResultado)++;
+        resultado[*sizeResult] = array1[i];
+        (*sizeResult)++;
     }
  
-    for (i = 0; i < tamanho2; i++) {
+    for (i = 0; i < size2; i++) {
         if (!estaPresente(array1, size1, array2[i])) {
-            resultado[*tamanhoResultado] = array2[i];
-            (*tamanhoResultado)++;
+            resultado[*sizeResult] = array2[i];
+            (*sizeResult)++;
         }
     }
 }
-void intersecao(int array1[], int size1, int array2[], int tamanho2, int resultado[], int *tamanhoResultado) {
+void intersecao(int array1[], int size1, int array2[], int size2, int resultado[], int *sizeResult) {
     int i;
     for (i = 0; i < size1; i++) {
-        if (estaPresente(array2, tamanho2, array1[i])) {
-            resultado[*tamanhoResultado] = array1[i];
-            (*tamanhoResultado)++;
+        if (estaPresente(array2, size2, array1[i])) {
+            resultado[*sizeResult] = array1[i];
+            (*sizeResult)++;
         }
     }
 }
-void diferenca(int array1[], int size1, int array2[], int tamanho2, int resultado[], int *tamanhoResultado) {
+void diferenca(int array1[], int size1, int array2[], int size2, int resultado[], int *sizeResult) {
     int i;
     for (i = 0; i < size1; i++) {
-        if (!estaPresente(array2, tamanho2, array1[i])) {
-            resultado[*tamanhoResultado] = array1[i];
-            (*tamanhoResultado)++;
+        if (!estaPresente(array2, size2, array1[i])) {
+            resultado[*sizeResult] = array1[i];
+            (*sizeResult)++;
         }
     }
 }
 int main() {
-    int size1, tamanho2;
+    int size1, size2;
     int array1[MAX_SIZE];
     int array2[MAX_SIZE];
-    int resultadoUniao[MAX_SIZE * 2]; 
+    int unionResult[MAX_SIZE * 2]; 
     int resultadoIntersecao[MAX_SIZE]; 
     int resultadodiferenca1[MAX_SIZE]; 
     int resultadodiferenca2[MAX_SIZE]; 
-    int tamanhoResultado = 0;
+    int sizeResult = 0;
     int i, j, k;
     printf("Entre com o numero de elementos do vetor 1: ");
     scanf("%d", &size1);
@@ -74,52 +74,52 @@ int main() {
         }
     }
     printf("\nEntre com o numero de elementos do vetor 2: ");
-    scanf("%d", &tamanho2);
-    if (tamanho2 <= 0) {
+    scanf("%d", &size2);
+    if (size2 <= 0) {
         printf("\nO numero de elementos do vetor deve ser maior do que zero\n");
         printf("\nEntre com o numero de elementos do vetor 2: ");
-    scanf("%d", &tamanho2);
+    scanf("%d", &size2);
     }
     printf("\nEntre com os elementos do vetor 2: ");
-    for (i = 0; i < tamanho2; i++) {
+    for (i = 0; i < size2; i++) {
         scanf("%d", &array2[i]);
     }
-    for (i = 0; i < tamanho2 - 1; i++) {
-        for (j = i + 1; j < tamanho2; j++) {
+    for (i = 0; i < size2 - 1; i++) {
+        for (j = i + 1; j < size2; j++) {
             while (array2[i] == array2[j]) {
                 printf("\nO vetor nao pode ter elementos repetidos\n");
                 printf("\nEntre com os elementos do vetor 2: ");
-    for (i = 0; i < tamanho2; i++) {
+    for (i = 0; i < size2; i++) {
         scanf("%d", &array2[i]);
     }
                 
             }
         }
     }
-    uniao(array1, size1, array2, tamanho2, resultadoUniao, &tamanhoResultado);
+    uniao(array1, size1, array2, size2, unionResult, &sizeResult);
     printf("\nVetor que representa a uniao dos vetores de entrada:");
-    for (i = 0; i < tamanhoResultado; i++) {
-        printf(" %d", resultadoUniao[i]);
+    for (i = 0; i < sizeResult; i++) {
+        printf(" %d", unionResult[i]);
     }
     
-    tamanhoResultado = 0;
-    intersecao(array1, size1, array2, tamanho2, resultadoIntersecao, &tamanhoResultado);
+    sizeResult = 0;
+    intersecao(array1, size1, array2, size2, resultadoIntersecao, &sizeResult);
     printf("\n\nVetor que representa a interseccao dos vetores de entrada:");
-    for (i = 0; i < tamanhoResultado; i++) {
+    for (i = 0; i < sizeResult; i++) {
         printf(" %d", resultadoIntersecao[i]);
     }
     
-    tamanhoResultado = 0;
-    diferenca(array1, size1, array2, tamanho2, resultadodiferenca1, &tamanhoResultado);
+    sizeResult = 0;
+    diferenca(array1, size1, array2, size2, resultadodiferenca1, &sizeResult);
     printf("\n\nVetor que representa a diferenca entre o vetor 1 e o vetor 2:");
-    for (i = 0; i < tamanhoResultado; i++) {
+    for (i = 0; i < sizeResult; i++) {
         printf(" %d", resultadodiferenca1[i]);
     }
     
-    tamanhoResultado = 0;
-    diferenca(array2, tamanho2, array1, size1, resultadodiferenca2, &tamanhoResultado);
+    sizeResult = 0;
+    diferenca(array2, size2, array1, size1, resultadodiferenca2, &sizeResult);
     printf("\n\nVetor que representa a diferenca entre o vetor 2 e o vetor 1:");
-    for (i = 0; i < tamanhoResultado; i++) {
+    for (i = 0; i < sizeResult; i++) {
         printf(" %d", resultadodiferenca2[i]);
     }
     printf("\n");
