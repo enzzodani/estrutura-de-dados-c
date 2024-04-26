c99530893a25591ad6bb4bb8a9fbdf070d80b290
enzzodani <gomes.enzodani@gmail.com>
2024-04-26 14:17:46 -0300

    basic doc 2

--- lista2-tad/data.c
+++ lista2-tad/data.c
@@ -10,74 +10,78 @@ struct data{ //Declaração do struct data
   unsigned int ano;
 };
 
-//Função que cria uma data dinâmicamente
-Data* criaData(unsigned int dia, unsigned int mes, unsigned int ano) {
-  Data *d = (Data *) malloc(sizeof(Data));
+//Funções auxiliares
 
-  if (d != NULL) {
-    d -> dia = dia;
-    d -> mes = mes;
-    d -> ano = ano;
-  }
-  return (d);
-}
+//Funções principais
 
-//Libera a memória da data criada dinâmicamente
-void liberaData(Data *d) {
-  if(d != NULL) {
-    free(d);
+  //Função que cria uma data dinâmicamente
+  Data* criaData(unsigned int dia, unsigned int mes, unsigned int ano) {
+    Data *d = (Data *) malloc(sizeof(Data));
+
+    if (d != NULL) {
+      d -> dia = dia;
+      d -> mes = mes;
+      d -> ano = ano;
+    }
+    return (d);
   }
 
-//Função que soma dias em uma data e retorna uma nova data
-Data* somaDiasData(Data d, unsigned int dias){
-  Data *novoDia = (Data *) malloc(sizeof(Data));
+  //Libera a memória da data criada dinâmicamente
+  void liberaData(Data *d) {
+    if(d != NULL) {
+      free(d);
+    }
+
+  //Função que soma dias em uma data e retorna uma nova data
+  Data* somaDiasData(Data d, unsigned int dias){
+    Data *novoDia = (Data *) malloc(sizeof(Data));
+    }
+
+  //Função que subtrai dias em uma data e retorna uma nova data
+  Data *subtrairDiasData(Data d, unsigned int dias) {
+    Data *novoDia = (Data *) malloc(sizeof(Data));
+    }
+
+  //Função que atribui uma nova data à uma data passada e caso falhe, transforma a data passada em NULL
+  void atribuirData(Data *d, unsigned int dia, unsigned int mes, unsigned int ano) {
+      
+      d -> dia = dia;
+      d -> mes = mes;
+      d -> ano = ano;
+   
+    }
+
+  //Função que obtém o dia da data
+  unsigned int obtemDiaData(Data d) {
+    return d.dia;
+    }
+
+  //Função que obtém o mes da Data
+  unsigned int obtemMesData(Data d) {
+    return d.mes;
   }
 
-//Função que subtrai dias em uma data e retorna uma nova data
-Data *subtrairDiasData(Data d, unsigned int dias) {
-  Data *novoDia = (Data *) malloc(sizeof(Data));
-  }
+  //Função que obtém o ano da Data
+  unsigned int obtemAnoData(Data d) {
+    return d.ano;
+    }
 
-//Função que atribui uma nova data à uma data passada e caso falhe, transforma a data passada em NULL
-void atribuirData(Data *d, unsigned int dia, unsigned int mes, unsigned int ano) {
+  //Função que identifica se a data pertence à um ano bissexto
+  unsigned int bissextoData(Data d) {
     
-    d -> dia = dia;
-    d -> mes = mes;
-    d -> ano = ano;
- 
-  }
+    }
 
-//Função que obtém o dia da data
-unsigned int obtemDiaData(Data d) {
-  return d.dia;
-  }
+  //Função que retorna -1 se a d1 < d2, 0 se d1=d2, e 1 se d1>d2
+  int comparaData(Data d1, Data d2) {
 
-//Função que obtém o mes da Data
-unsigned int obtemMesData(Data d) {
-  return d.mes;
-}
+    }
 
-//Função que obtém o ano da Data
-unsigned int obtemAnoData(Data d) {
-  return d.ano;
-  }
+  //Função que retorna o número de dias entre as datas
+  unsigned int numeroDiasData(Data d1, Data d2) {
+    
+    }
 
-//Função que
-unsigned int bissextoData(Data d) {
-  
-  }
+  //Função que imprime a data passada em uma formatação especifica
+  char* imprimeData(Data d, char *formato) {
 
-//Função que
-int comparaData(Data d1, Data d2) {
-
-  }
-
-//Função que
-unsigned int numeroDiasData(Data d1, Data d2) {
-  
-  }
-
-//Função que imprime a data passada em uma formatação especifica
-char* imprimeData(Data d, char *formato) {
-
-  }
+    }
