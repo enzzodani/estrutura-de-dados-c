1ac7fa419ebaf37b0ef674bf88ab73ac0225edba
enzzodani <gomes.enzodani@gmail.com>
2024-04-26 11:56:27 -0300

    doc basics

--- lista2-tad/data.c
+++ lista2-tad/data.c
@@ -4,12 +4,13 @@
 #include <math.h>
 #include "data.h"
 
-struct data{
+struct data{ //Declaração do struct data
   unsigned int dia;
   unsigned int mes;
   unsigned int ano;
 };
 
+//Função que cria uma data dinâmicamente
 Data* criaData(unsigned int dia, unsigned int mes, unsigned int ano) {
   Data *d = (Data *) malloc(sizeof(Data));
 
@@ -21,19 +22,23 @@ Data* criaData(unsigned int dia, unsigned int mes, unsigned int ano) {
   return (d);
 }
 
+//Libera a memória da data criada dinâmicamente
 void liberaData(Data *d) {
   if(d != NULL) {
     free(d);
   }
 
+//Função que soma dias em uma data e retorna uma nova data
 Data* somaDiasData(Data d, unsigned int dias){
   Data *novoDia = (Data *) malloc(sizeof(Data));
   }
-  
+
+//Função que subtrai dias em uma data e retorna uma nova data
 Data *subtrairDiasData(Data d, unsigned int dias) {
   Data *novoDia = (Data *) malloc(sizeof(Data));
   }
 
+//Função que atribui uma nova data à uma data passada e caso falhe, transforma a data passada em NULL
 void atribuirData(Data *d, unsigned int dia, unsigned int mes, unsigned int ano) {
     
     d -> dia = dia;
@@ -42,30 +47,37 @@ void atribuirData(Data *d, unsigned int dia, unsigned int mes, unsigned int ano)
  
   }
 
+//Função que obtém o dia da data
 unsigned int obtemDiaData(Data d) {
   return d.dia;
   }
 
+//Função que obtém o mes da Data
 unsigned int obtemMesData(Data d) {
   return d.mes;
 }
 
+//Função que obtém o ano da Data
 unsigned int obtemAnoData(Data d) {
   return d.ano;
   }
 
+//Função que
 unsigned int bissextoData(Data d) {
   
   }
 
+//Função que
 int comparaData(Data d1, Data d2) {
 
   }
 
+//Função que
 unsigned int numeroDiasData(Data d1, Data d2) {
   
   }
 
+//Função que imprime a data passada em uma formatação especifica
 char* imprimeData(Data d, char *formato) {
 
   }
