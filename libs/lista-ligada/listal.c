46cb3030a0859e9c0f1f3236932330392d7abdf1
enzzodani <gomes.enzodani@gmail.com>
2024-06-21 10:37:59 -0300

    listal.c definition functions

--- libs/lista-ligada/listal.c
+++ libs/lista-ligada/listal.c
@@ -53,3 +53,45 @@ void insertAtEnd(Node** head, int data) {
 void insertAtPosition (Node** head, int data, int position) {
 
 }
+
+//Removal
+  //atBeginnig
+void removalAtBeginnig (Node** head) {
+
+}
+  //atEnd
+void removalAtEnd (Node** head) {
+
+}
+  //atPosition
+void removalAtPosition (Node** head, int position) {
+
+}
+
+//Search
+  //Value
+Node searchAtValue (Node* head, int data) {
+
+}
+  //atPosition
+Node searchAtPosition (Node* head, int position) {
+
+}
+
+
+//Update
+  //atPosition
+void updateAtPosition (Node* head, int data, int position) {
+
+}
+
+//Traversal
+  //printList
+void printList (Node* head) {
+
+}
+  //printListInfo
+void printListInfo (Node* head) {
+
+}
+
