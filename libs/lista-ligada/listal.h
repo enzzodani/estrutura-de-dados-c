cab7ca71001cbb9cbfbbd5133c9189b9438cdadd
enzzodani <gomes.enzodani@gmail.com>
2024-06-21 07:58:20 -0300

    listal.h operations

---
+++ libs/lista-ligada/listal.h
@@ -0,0 +1,30 @@
+#ifndef LISTA_LIGADA_H
+#define LISTA_LIGADA_H
+
+#include <stdbool.h>
+
+//Definition Node
+
+//Linked List Functions
+  //Insert
+    //atBeginnig
+    //atEnd
+    //atPosition
+
+
+  //Removal
+    //atBeginnig
+    //atEnd
+    //atPosition
+
+  //Search
+    //Value
+    //atPosition
+    //ListInformation
+
+  //Update
+    //Value
+
+  //Traversal
+    //printList
+#endif 
