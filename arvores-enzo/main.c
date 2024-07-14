7d1fe040252ef5a153dfb66454dc1cbf337119c3
enzzodani <gomes.enzodani@gmail.com>
2024-07-14 16:44:24 -0300

    arvore esboco

--- arvores-enzo/main.c
+++ arvores-enzo/main.c
@@ -9,35 +9,81 @@ struct Node {
   Node* direita;
 };
 
-//Node structure
-Node* Root = NULL;
-
 //Functions Declaration
 Node* criarArvore(int chave);
 
-void inserirNoArvore(Node* newNode);
+void inserirNoArvore(int chave);
 
 void removerNoArvore(int chave);
 
-Node* obterNovalor(int chave);
+Node* obterNovalor(Node* raiz, int chave);
 
-void listarArvoreOrdem();
+void listarArvoreOrdem(Node* raiz);
 
-void listarArvorePreOrdem();
+void listarArvorePreOrdem(Node* raiz);
 
-void listarArvorePosOrdem();
+void listarArvorePosOrdem(Node* raiz);
 
-Node* obtervalorMaximoArvore();
+Node* obtervalorMaximoArvore(Node* raiz);
 
-Node* obtervalorMinimoArvore();
+Node* obtervalorMinimoArvore(Node* raiz);
 
-Node* obterSucessorNo();
+Node* obterSucessorNo(int chaveSucessora);
 
-Node* obterAntecessorNo();
+Node* obterAntecessorNo(Node* raiz);
+
+//Auxliar Functions
+Node* inserirRecursivo(Node* noAtual, int chave);
+
+//Root of the Tree (must to be a global variable)
+Node* raizArvore = NULL;
 
 //Main
 int main(int argc, char *argv[])
 {
+  int operacao;
+
+  switch (operacao) {
+    case 1:
+    raizArvore = criarArvore(chave);
+    break;
+
+    case 2:
+    break;
+    
+    case 3:
+    break;
+    
+    case 4:
+    break;
+    
+    case 5:
+    break;
+    
+    case 6:
+    break;
+    
+    case 7:
+    break;
+    
+    case 8:
+    break;
+    
+    case 9:
+    break;
+    
+    case 10:
+    break;
+    
+    case 11:
+    break;
+    
+    case 0:
+    break;
+    
+    default:
+
+  }
 
   return 0;
 }
@@ -47,7 +93,7 @@ Node* criarArvore(int chave) {
   Node* raiz = (Node *) malloc(sizeof(Node));
 
   if (raiz == NULL) {
-    free(raiz);
+    puts("Erro na alocacao de memoria no criarArvore");
     return NULL;
   }
 
@@ -56,6 +102,7 @@ Node* criarArvore(int chave) {
   raiz -> direita = NULL;
 
   return raiz;
+
 }
 
 void inserirNoArvore(int chave) {
@@ -66,5 +113,26 @@ void inserirNoArvore(int chave) {
     return;
   }
 
+  Node* noAtual = raizArvore;
+
+  while(1) {
+    if (novoNo < noAtual) { 
+      if (noAtual -> esquerda == NULL) {
+        noAtual -> esquerda = novoNo;
+        return;
+      } else {
+        noAtual = noAtual -> esquerda;
+      }
+    } else if (novoNo > noAtual) {
+      if (noAtual -> direita == NULL) {
+        noAtual -> direita = novoNo;
+        return;
+      } else {
+        noAtual = noAtual -> direita;
+      }
+    } else {
+      return;
+    }
+  }
   
 }
