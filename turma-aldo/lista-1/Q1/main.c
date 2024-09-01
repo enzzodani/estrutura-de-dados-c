d638c4b1e809307614e0afe1153d04db3e5e740e
enzzodani <gomes.enzodani@gmail.com>
2024-09-01 16:03:15 -0300

    Feito: acha para anos maiores

--- turma-aldo/lista-1/Q1/main.c
+++ turma-aldo/lista-1/Q1/main.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
+#include <math.h>
+
+int anoMaior (int anoAtual) {
+  int diferenca, prox, somaNecessaria, quociente;
+
+    if (anoAtual > 1986) {
+        diferenca = anoAtual - 1986;
+        if (diferenca < 76) {
+          somaNecessaria = 76 - diferenca;
+          prox = somaNecessaria + anoAtual;
+          return prox;
+        } else if (diferenca > 76) {
+            quociente = floor(diferenca/76);
+            prox = 76*(quociente+1) +1986;
+        } else {
+            prox = anoAtual + 76;
+            return prox;
+        }
+    }
+}
 
 int main() {
   
-  :
+  int anoAtual;
 
+
+  puts("Insira o ano desejado: ");
+    scanf("%d", &anoAtual);
+
+  printf("O proximo cometa passara em %d \n", anoMaior(anoAtual));  
 
     return 0;
 }
