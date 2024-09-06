22dbc7adba87b3f1c0c95d5a285e525723cc45f3
enzzodani <gomes.enzodani@gmail.com>
2024-09-05 23:25:14 -0300

    vetor finalizado

--- turma-aldo/lista-1/Q9/main.c
+++ turma-aldo/lista-1/Q9/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
-    return 0;
+  int t; //Definição do numero de testes
+  scanf("%d", &t);
+
+  double* num = (double *) malloc(2 * t * sizeof(double));
+
+  for (int i = 0; i < t; i++) {
+    double a, b;
+    scanf("%lf %lf", &a, &b);
+    num[i] = (a * b);
+  }
+
+  for (int i = 0; i < t; i++) {
+    printf("%lf\n", num[i]);
+  }
+
+  return 0;
 }
